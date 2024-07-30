e6c73b55ae3fe4ec40a9bb3dee14ad408d15838f
Alex-Surkov <127119419+Alex-Surkov@users.noreply.github.com>
2024-07-30 19:35:09 +0300

    initial commit

---
+++ src/test/java/com/github/Alex_Surkov312/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.Alex_Surkov312;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
